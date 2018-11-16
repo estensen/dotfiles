95dda94b45bdda2e3d87fa584e8c38d4e8c8c2d5
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-16 10:42:21 +0100

    Add homebrew

---
+++ brew.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+brew install wget
+brew install gnupg
+brew install vim
+brew install grep
+brew install openssh
+brew install git
+
+brew cask install alfred
+brew cask install dropbox
+brew cask install firefox
+brew cask install google-chrome
+brew cask install spotify
+
+# Remove outdated versions from the cellar.
+brew cleanup
