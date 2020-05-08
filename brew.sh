31410b70483c9ef20a5188b244b9698bc7955433
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-05-08 15:56:38 +0200

    Remove unused formulaes

--- brew.sh
+++ brew.sh
@@ -22,15 +22,6 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-brew install wget
-brew install gnupg
-brew install vim
-brew install grep
-brew install openssh
-brew install git
-brew install gibo
-
-brew cask install alfred
 brew cask install dropbox
 brew cask install firefox
 brew cask install google-chrome
