e2f2952f2ffd4b3fa8cf5c1b16ea203223a9ad0c
Håvard Anda Estensen <haavard.ae@gmail.com>
2021-11-22 10:57:07 +0100

    Update Homebrew install path

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install and upgrade Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew upgrade
 
