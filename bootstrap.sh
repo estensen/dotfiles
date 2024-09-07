7c96ccedce68e99027e6fc34fbd7738218c24526
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-09-07 09:18:53 +0200

    Install powerlevel10k

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,10 @@ rsync --exclude ".git/" \
 	--exclude "bootstrap.sh" \
 	--exclude "README.md" \
 	-avh --no-perms . ~;
+
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+ 
 source ~/.zshrc;
 
